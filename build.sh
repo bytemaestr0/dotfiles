c3447c154cbc94050e3d02e6baec238fee0d947e
bytemaestr0 <cipikali7@gmail.com>
2024-03-16 17:53:37 +0000

    fixed nvim for root

--- build.sh
+++ build.sh
@@ -4,28 +4,35 @@ dotfiles="$(dirname "$(readlink -f "$0")")"
 
 sudo apt update -y && sudo apt full-upgrade -y
 
-sudo snap install nvim --classic
+sudo apt install git -y
 
 sudo apt install zsh -y
 
+sudo apt-get install ninja-build gettext cmake unzip curl build-essential -y
+
 sudo chsh -s /usr/bin/zsh $USER
+
+git clone https://github.com/neovim/neovim
+
+cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+
+sudo make install
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-sudo cp -r $dotfiles/konsole/. /home/$USER/
+sudo cp -r $dotfiles/konsole/. $HOME/
  
-sudo cp -r $dotfiles/zsh/. /home/$USER/
+sudo cp -r $dotfiles/zsh/. $HOME/
 
-sudo cp -r $dotfiles/.ssh/. /home/$USER/
+sudo cp -r $dotfiles/ssh/. $HOME/
 
-sudo cp -r $dotfiles/nvim/. /home/$USER/
+sudo cp -r $dotfiles/nvim/. $HOME/
 
-clear
+cd ..
 
-echo ""
-echo ""
-echo ""
+rm -rf neovim
+
+clear 
+
 echo "done"
-echo ""
-echo ""
-echo ""
+
